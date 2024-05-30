62be3ecbd9adb91cb348090700d28d38d9cd93cc
Saikat <saikatchakravortyofficial@gmail.com>
2024-05-30 16:32:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,22 +10,55 @@ const skull = document.getElementById("skull")
 const footer = document.getElementById("footer");
 const colors = ["#294192", "#2f4d0d", "#790979", "#811414"];
 const calcValues = {
-    num1: "1", num2: "2", num3: "3", num4: "4", num5: "5", num6: "6",
-    num7: "7", num8: "8", num9: "9", num0: "0", comma: ",", sum: "+",
-    substract: "-", divide: "÷", multiply: "x", factorial: "!", overX: "1/",
-    equalTo: "=", squareRoot: "√", cubeRoot: "∛", squarePower: "²",
-    pi: "3,1415926535897932384", euler: "2,7182818284590452353"};
+    num1: "1", 
+    num2: "2", 
+    num3: "3", 
+    num4: "4", 
+    num5: "5", 
+    num6: "6",
+    num7: "7", 
+    num8: "8", 
+    num9: "9", 
+    num0: "0", 
+    comma: ",", 
+    sum: "+",
+    substract: "-", 
+    divide: "÷", 
+    multiply: "x", 
+    factorial: "!", 
+    overX: "1/",
+    equalTo: "=", 
+    squareRoot: "√", 
+    cubeRoot: "∛", 
+    squarePower: "²",
+    pi: "3,1415926535897932384", 
+    euler: "2,7182818284590452353"};
 const keyboardSymbols = {
-    "1": "num1", "2": "num2", "3": "num3", "4": "num4", "5": "num5",
-    "6": "num6", "7": "num7", "8": "num8", "9": "num9", "0": "num0",
-    "Backspace": "del1", "Delete": "clear", ",": "comma",
-    "+": "sum", "-": "substract", "/": "divide", "*": "multiply",
-    "Enter": "equalTo", "=": "equalTo", "e": "euler"
+    "1": "num1", 
+    "2": "num2", 
+    "3": "num3", 
+    "4": "num4", 
+    "5": "num5",
+    "6": "num6", 
+    "7": "num7", 
+    "8": "num8", 
+    "9": "num9", 
+    "0": "num0",
+    "Backspace": "del1", 
+    "Delete": "clear", 
+    ",": "comma",
+    "+": "sum", 
+    "-": "substract", 
+    "/": "divide", 
+    "*": "multiply",
+    "Enter": "equalTo", 
+    "=": "equalTo", 
+    "e": "euler"
 }
-var givenResult = false;
-var resultValue = "0";
-var colorNum = 0;
-var record = [];
+let givenResult = false;
+let resultValue = "0";
+let colorNum = 0;
+let record = [];
 
 
 function setUp() {
